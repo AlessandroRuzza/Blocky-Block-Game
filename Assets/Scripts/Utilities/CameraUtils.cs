0f2d4f0c2b470a92f1ce53f77068d596d9bdba46
Alessandro Ruzza <alessandroruzza22@gmail.com>
2021-11-11 01:14:45 +0100

    Made CameraUtils

---
+++ Assets/Scripts/Utilities/CameraUtils.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraUtils : MonoBehaviour
+{
+    float halfScreenHeight, halfScreenWidth;
+
+    public float halfWidth {get{ return halfScreenWidth; }}
+    public float halfHeight {get{ return halfScreenHeight; }}
+}
